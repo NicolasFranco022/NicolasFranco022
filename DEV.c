8dec6bcd9528ee746007162ce07461ee2b84ebbe
NicolasFranco022 <143292684+NicolasFranco022@users.noreply.github.com>
2023-10-05 16:48:30 -0300

    Update DEV.c

--- DEV.c
+++ DEV.c
@@ -5,6 +5,13 @@
 int main(){
 
     printf("testee");
+    https://www.youtube.com/watch?v=wGITRyijsuo
+    https://www.youtube.com/watch?v=v1zakuL-LHE
+    https://youtube.com/watch?v=FD0bi4H08IU
+    https://www.youtube.com/watch?v=_byMWMrwEdU
+    https://www.youtube.com/watch?v=TnS_ZLXuY2s
+    https://www.youtube.com/watch?v=eriDnpkh5kA
+    https://www.youtube.com/watch?v=yB6oD6DRXdY
 
     return 0;
 }
