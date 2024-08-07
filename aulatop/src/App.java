8b7afb154be8ae84f318a5fd3cc1b97a47e6952d
nckfranco <143292684+NicolasFranco022@users.noreply.github.com>
2024-08-07 10:36:07 -0300

    a

---
+++ aulatop/src/App.java
@@ -0,0 +1,18 @@
+import modelo.Carro;
+
+public class App {   
+    public static void main(String[] args) throws Exception {
+    Carro carro1 = new Carro();
+    carro1.ano = 1980;
+    carro1.modelo = "Fusca";
+    
+    
+    System.out.println(carro1.exibir());
+    System.out.println("---------------------------");
+    
+
+    Carro carro2  = new Carro();
+    
+    n 
+    }
+}
